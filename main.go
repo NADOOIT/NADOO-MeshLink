4cb2f4de2e622ef71b95505b243cd2756b1d96c1
Christoph Backhaus <christoph.backhaus@nadooit.de>
2023-12-16 21:01:03 +0100

    Update main.go
    Added local networkadress lookup and changed the adress to use that. The idea is that a diffrent pc in the network should be able to connect this way

--- main.go
+++ main.go
@@ -7,19 +7,47 @@ import (
 	"os/signal"
 	"syscall"
 
+	"net"
+
 	"github.com/libp2p/go-libp2p"
 	peerstore "github.com/libp2p/go-libp2p/core/peer"
 	"github.com/libp2p/go-libp2p/p2p/protocol/ping"
 	multiaddr "github.com/multiformats/go-multiaddr"
 )
 
+func getLocalIP() string {
+	addrs, err := net.InterfaceAddrs()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	for _, address := range addrs {
+		// Check the address type and if it is not a loopback, then display it
+		if ipnet, ok := address.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
+			if ipnet.IP.To4() != nil {
+				return ipnet.IP.String()
+			}
+		}
+	}
+
+	return ""
+}
+
 func main() {
+	localIP := getLocalIP()
+	if localIP == "" {
+		fmt.Println("Could not find a non-loopback IP address on your machine.")
+		os.Exit(1)
+	}
+	fmt.Printf("Your local IP address: %s\n", localIP)
     // start a libp2p node that listens on a random local TCP port,
     // but without running the built-in ping protocol
     node, err := libp2p.New(
-        libp2p.ListenAddrStrings("/ip4/127.0.0.1/tcp/0"),
+        libp2p.ListenAddrStrings("/ip4/" + localIP + "/tcp/0"),
         libp2p.Ping(false),
     )
+
     if err != nil {
         panic(err)
     }
